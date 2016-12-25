fd9f71257000b96cd05c39c8e10a1ef90d952d31
David Guan <DavidGuanDev@Gmail.com>
2016-12-25 23:12:58 +0800

    Update README

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-## Nothing bug a project boilerplate
+![](https://cloud.githubusercontent.com/assets/10692276/21471740/3a7b2188-caf7-11e6-8743-87a119548a11.png)
+
+## TODO list:
+- [ ] Firebase integration(Dec 26 night)
+- [ ] Dashboard(Dec 27 night)
+- [ ] Improve pong part(Dec 28 night)
+
+## Local development
+```
+npm install && npm start
+```
--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ const KEY_DOWN = 40;
 const context = document.querySelector('#app').getContext('2d');
 
 context.fillStyle = '#fff';
+context.strokeStyle = '#fff';
+context.setLineDash([5, 15]);
+context.textAlign = 'center';
+context.font = '50px Arial';
 const leftPaddle = new Paddle(context, true);
 const rightPaddle = new Paddle(context, false);
 const ball = new Ball(context, leftPaddle, rightPaddle);
@@ -28,7 +32,7 @@ if (requestAnimationFrame) {
     setTimeout(() => {
       requestAnimationFrame(draw);
       context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-
+      resetBoard();
       ball.update();
       leftPaddle.update(movDir);
       rightPaddle.update(movDir);
@@ -37,4 +41,14 @@ if (requestAnimationFrame) {
   requestAnimationFrame(draw);
 } else {
   console.error('requestAnimationFrame isn\'t supportted');
+}
+
+function resetBoard() {
+  context.beginPath();
+  context.moveTo(200, 0);
+  context.lineTo(200, 400);
+  context.stroke();
+  // TODO: replace this mock data
+  context.fillText('1', 100, 55);
+  context.fillText('3', 300, 55);
 }
